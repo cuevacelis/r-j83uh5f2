85e8be0b44d3066d6758a278e810cc12e1a9859a
Jose Cueva Celis <cuevacelis@hotmail.com>
2021-12-15 10:42:22 -0500

    :)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+  countCharter = (e) => {
+    //e.persist()
+    this.setState({ count: e.target.value.length })
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={(e) => this.countCharter(e)}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
